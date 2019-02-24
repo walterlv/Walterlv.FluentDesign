62625a756d7f6982cca83375dd753c30bbf01625
walterlv <xblero@gmail.com>
2019-02-24 10:10:14 +0800

    Add project files.

---
+++ Walterlv.FluentDesign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.FluentDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
